4de5ae125d21365e21b221bd54c9046c1b08f8e3
NaghamSabry <161896009+NaghamSabry@users.noreply.github.com>
2026-07-10 21:04:31 +0300

    Update cnn_temporal_model.py

--- ml_core/src/models/cnn_temporal_model.py
+++ ml_core/src/models/cnn_temporal_model.py
@@ -1,4 +1,3 @@
-%%writefile cnn_temporal_model.py
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import TimeDistributed, Conv2D, MaxPooling2D, Flatten, LSTM, GRU, Dense, Dropout, BatchNormalization
